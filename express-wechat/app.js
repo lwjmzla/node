03bfdcbd797c5045d3bc161eb6e9b4ef0d2521a1
lwjmzla <214245088@qq.com>
2019-11-10 23:11:10 +0800

    update

--- express-wechat/app.js
+++ express-wechat/app.js
@@ -20,17 +20,19 @@ app.use(postBody.json()) // 解析json格式数据(application/json)
 app.use(cookieParser('lwj')) // 相当于 app.use('/', cookieParser('lwj')) 
 
 //设置跨域访问
-app.all('*', function(req, res, next) {
-  // console.log(req.headers.origin)
-  if (req.headers.origin && req.headers.origin.indexOf('localhost') > -1) { // !只允许localhost的进行跨域访问
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
+app.all('*',function (req, res, next) {
+  //if (req.headers.origin && req.headers.origin.indexOf('localhost') > -1) ) {} // !只允许localhost的进行跨域访问
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  if (req.method == 'OPTIONS') { // !标明跨域支持option请求方法
+    res.send(200); //让options请求快速返回/
   }
-  next();
-})
+  else {
+    next();
+  }
+});
 
 const config = {
   token: 'tokenabc123',
@@ -38,7 +40,7 @@ const config = {
   appsecret: '05dc1e5e0cd3017b8706242eff8a826d'
 }
 // !微信验证本人服务器有效性  
-app.use(auth())
+app.use(auth()) // !加了这个后 './routes/index.js'的路由不生效了，都被拦截了。
 
 
 
@@ -50,7 +52,8 @@ app.use(function (req, res, next) { //!其实 就是省略 '/'
 })
 // !统一处理next(err)  放在404后面
 app.use('/', function (err, req, res, next) {
-  res.send('获取文件失败')
+  //res.send('获取文件失败') // !比如访问/test123出现了错误Error: Can't set headers after they are sent
+  res.end('获取文件失败') // !上面错误的解决方式1.把send改为end   2.在类似的res.send()等函数也要加return  例如在 auth.js 改成return res.end('error') 全部
 })
 
 app.listen(3335, () => {
